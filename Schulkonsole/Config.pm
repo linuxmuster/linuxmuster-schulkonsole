0ca3902fb93c8a6eced1a15ee82b37b1f26f1166
Frank Schütte <fschuett@gymnasium-himmelsthuer.de>
2016-07-02 10:11:32 +0200

    require linuxmuster-base 6.2.0, sophomorix 2.4.59 to reflect linuxmuster.net 6.2

--- Schulkonsole/Config.pm
+++ Schulkonsole/Config.pm
@@ -1432,7 +1432,7 @@ $false= 'no';
 $on = 'on';
 $off = 'off';
 
-$_version = '0.35.1';
+$_version = '0.36.0';
 $_sysconfdir = '/etc/linuxmuster/schulkonsole';
 $_templatedir = '/usr/share/schulkonsole/tt';
 $_runtimedir = '/var/lib/schulkonsole';
